9d4db16985cba0938abd8f6fa0dfa919c1a041a7
Wanzzy1 <arcwahdan@gmail.com>
2024-01-24 14:49:03 +0700

    membuat layouts dashboard dan membuat 7 tabel migrations

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,26 @@
+
+
+   @extends('layouts/app')
+   @section('content')
+
+
+    <div class="row">
+<h1>welcome to Dashboard</h1>
+                        <!-- Earnings (Monthly) Card Example -->
+                    </div>
+
+                    <!-- Content Row -->
+
+                    <div class="row">
+
+                        <!-- Area Chart -->
+
+                    </div>
+
+                    <!-- Content Row -->
+                    <div class="row">
+
+                        <!-- Content Column -->
+
+                    </div>
+                     @endsection
